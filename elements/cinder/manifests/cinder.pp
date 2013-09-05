3576e8d16703c12b54ca1b5216bbc70d951651f0
Lucas Alvares Gomes <lucasagomes@gmail.com>
2013-09-05 11:49:30 +0100

    Adding cinder puppet element

---
+++ elements/cinder/manifests/cinder.pp
@@ -0,0 +1,24 @@
+
+class {"cinder":
+	sql_connection	=> "$::cinder_db",
+  	rabbit_password	=> "$::rabbit_password",
+    	rabbit_host     => "$::rabbit_host",
+	debug          	=> true,
+    	verbose		=> true,
+}
+
+class { 'cinder::api':
+	keystone_auth_host => "$::keystone_host",
+	keystone_user => "cinder",
+	keystone_password => "$::service_password",
+	enabled => $enabled,
+}
+
+class { 'cinder::volume': }
+
+class { 'cinder::scheduler': }
+
+# Needed on debian based distros
+file{'/etc/default/iscsitarget':
+  content => 'ISCSITARGET_ENABLE=true',
+}
