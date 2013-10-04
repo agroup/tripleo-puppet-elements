d4f8457dbe8ba3124b15b877819a9a2d9725091a
Derek Higgins <derekh@redhat.com>
2013-10-04 10:48:20 +0100

    Configure/Start neutron metadata agent

--- elements/neutron-network-node/manifests/neutron-nnn.pp
+++ elements/neutron-network-node/manifests/neutron-nnn.pp
@@ -8,5 +8,14 @@ class {'neutron::agents::l3':
 class {'neutron::agents::dhcp':
 }
 
+class {'neutron::agents::metadata':
+  auth_password => "$::neutron_service_password",
+  shared_secret => "",
+  auth_tenant    => 'service',
+  auth_user      => 'neutron',
+  auth_region    => 'regionOne',
+}
+
+
 #package{'iputils':}
 
