477035e3a398b75270eb510f594ac76ff5c89bd9
Richard Su <rwsu@redhat.com>
2013-10-17 15:22:31 -0700

    Pass metadata, waitcondition, and watch server urls to heat-engine

--- elements/heat-engine/manifests/heat-engine.pp
+++ elements/heat-engine/manifests/heat-engine.pp
@@ -1,3 +1,6 @@
 class {"heat::engine":
     enabled => $enabled,
+    heat_metadata_server_url => "$::heat_metadata_server_url",
+    heat_waitcondition_server_url => "$::heat_waitcondition_server_url",
+    heat_watch_server_url => "$::heat_heat_watch_server_url",
 }
